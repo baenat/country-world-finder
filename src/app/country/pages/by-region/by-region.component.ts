b7848b5ee48d9c11d74a699b5f5b91b0abd9a5bf
Baenat <bae.julian10@gmail.com>
2025-09-22 16:45:04 -0500

    Añade pagina para busqueda por region

---
+++ src/app/country/pages/by-region/by-region.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { DataListComponent } from "../../components/data-list/data-list.component";
+
+@Component({
+  selector: 'app-by-region',
+  imports: [DataListComponent],
+  templateUrl: './by-region.component.html',
+  styleUrl: './by-region.component.css'
+})
+export class ByRegionComponent {
+
+}
