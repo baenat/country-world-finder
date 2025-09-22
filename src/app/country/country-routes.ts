6ebf83afa279a59ecff76d8d13d30d9ed48ae6ed
Baenat <bae.julian10@gmail.com>
2025-09-22 16:45:28 -0500

    Actualiza rutas y componentes para incluir búsqueda por región y cambia texto de búsqueda por país

--- src/app/country/country-routes.ts
+++ src/app/country/country-routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ByCountryComponent } from './pages/by-country/by-country.component';
 import { LayoutCountryComponent } from './layout/layout-country.component';
+import { ByCapitalComponent } from './pages/by-capital/by-capital.component';
+import { ByRegionComponent } from './pages/by-region/by-region.component';
 
 export const countryRoutes: Routes = [
   {
@@ -9,7 +11,15 @@ export const countryRoutes: Routes = [
     children: [
       {
         path: 'by-capital',
+        component: ByCapitalComponent,
+      },
+      {
+        path: 'by-country',
         component: ByCountryComponent,
+      },
+      {
+        path: 'by-region',
+        component: ByRegionComponent,
       },
       {
         path: '**',
