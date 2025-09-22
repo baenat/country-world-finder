33bdbfdca78db0e5f044804ab9f9fd8af4466b4e
Baenat <bae.julian10@gmail.com>
2025-09-22 16:37:36 -0500

    Implementa componentes genericos buscar y listar información

--- src/app/country/pages/by-country/by-country.component.ts
+++ src/app/country/pages/by-country/by-country.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { SearchInputComponent } from "../../components/search-input/search-input.component";
+import { DataListComponent } from "../../components/data-list/data-list.component";
 
 @Component({
   selector: 'app-by-country',
-  imports: [],
+  imports: [SearchInputComponent, DataListComponent],
   templateUrl: './by-country.component.html',
   styleUrl: './by-country.component.css'
 })
 export class ByCountryComponent {
+
+  onEmitSearch(value: string) {
+    console.log({ value });
+  }
 
 }
